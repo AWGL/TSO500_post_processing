061abd40896137027dd5b01f7d940a6259d96bc3
Laura McCluskey <laura.mccluskey@wales.nhs.uk>
2021-07-23 16:25:17 +0100

    update filter_sample_list.py to create sample lists for rna and dna for each worksheet

--- filter_sample_list.py
+++ filter_sample_list.py
@@ -16,22 +16,38 @@ print(table)
 table=table.drop(["Referral_value"], axis=1)
 table=table.drop(["Description"], axis=1)
 
-worksheets=table["Sample_Plate"].unique()
+table_rna=(table[table["Sample_Type"]=="RNA"])
+table_dna=(table[table["Sample_Type"]=="DNA"])
+
+
+worksheets_rna=table_rna["Sample_Plate"].unique()
+worksheets_dna=table_dna["Sample_Plate"].unique()
+
 
 #print a list of worksheets to a file
-worksheets_file=open('worksheets.txt','w')
+worksheets_file_dna=open('worksheets_dna.txt','w')
 
-for worksheet in worksheets:
-     worksheets_file.write(worksheet)
-     worksheets_file.write('\n')
-worksheets_file.close()
-
-for worksheet in worksheets:
-	table_worksheet=(table[table["Sample_Plate"]==worksheet])
-
-	table_worksheet_RNA=(table_worksheet[table_worksheet["Sample_Type"]=="RNA"])
-	table_worksheet_RNA.to_csv("samples_correct_order_"+worksheet+"_RNA.csv", index=False)
+for worksheet in worksheets_dna:
+     worksheets_file_dna.write(worksheet)
+     worksheets_file_dna.write('\n')
+worksheets_file_dna.close()
 
 
-	table_worksheet_DNA=(table_worksheet[table_worksheet["Sample_Type"]=="DNA"])
-	table_worksheet_DNA.to_csv("samples_correct_order_"+worksheet+"_DNA.csv", index=False)
+#print a list of worksheets to a file
+worksheets_file_rna=open('worksheets_rna.txt','w')
+
+for worksheet in worksheets_rna:
+     worksheets_file_rna.write(worksheet)
+     worksheets_file_rna.write('\n')
+worksheets_file_rna.close()
+
+
+for worksheet in worksheets_rna:
+	table_rna_worksheet=table_rna
+	table_rna_worksheet=(table_rna[table_rna["Sample_Plate"]==worksheet])
+	table_rna_worksheet.to_csv("samples_correct_order_"+worksheet+"_RNA.csv", index=False, header=False)
+
+for worksheet in worksheets_dna:
+	table_dna_worksheet=table_dna
+	table_dna_worksheet=(table_dna[table_dna["Sample_Plate"]==worksheet])
+	table_dna_worksheet.to_csv("samples_correct_order_"+worksheet+"_DNA.csv", index=False, header=False)
