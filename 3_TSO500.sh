aa395ada3e7e46dc44e7eb9f7fccd53bcb769768
SophieS9 <sophie.shaw@wales.nhs.uk>
2022-09-21 09:59:29 +0100

    Changes to script 3 to be RNA only

--- 3_TSO500.sh
+++ 3_TSO500.sh
@@ -10,7 +10,7 @@
 #              end as NTC is required)
 # Use:         from /Output/results/<run_id> directory, run: 
 #              sbatch --export=raw_data=/data/archive/novaseq/<run_id> 3_TSO500.sh
-# Version:     1.0.6
+# Version:     1.0.7
 
 
 ##############################################################################################
@@ -45,7 +45,7 @@ set -euo pipefail
 #  Illumina app
 ##############################################################################################
 
-samples_to_gather=$(python $pipeline_scripts/gather_list.py $SLURM_SUBMIT_DIR/sample_list.txt $SLURM_SUBMIT_DIR)
+samples_to_gather=$(python $pipeline_scripts/gather_list.py $SLURM_SUBMIT_DIR/samples_correct_order_*_RNA.csv $SLURM_SUBMIT_DIR)
 
 # make sure to use singularity flag
 $app_dir/TruSight_Oncology_500_RUO.sh \
@@ -58,10 +58,6 @@ $app_dir/TruSight_Oncology_500_RUO.sh \
   --gather $samples_to_gather
 
 
-##############################################################################################
-#  Generate inputs for variants database - DNA
-##############################################################################################
-
 # activate conda env
 set +u
 conda activate TSO500_post_processing
@@ -69,57 +65,6 @@ set -u
 
 # make folder for database output
 mkdir Gathered_Results/Database
-
-# create variants and coverage tables in correct format to import to database
-for worksheet_id in $(cat worksheets_dna.txt); do
-
-    for line in $(cat samples_correct_order_"$worksheet_id"_DNA.csv); do
-        echo $line
-
-        sample="$(echo "$line" | cut -d, -f1)"
-        worksheet_id=$(echo "$line" | cut -d, -f2)
-        referral=$(echo "$line" | cut -d, -f4)
-
-        # format variants ready for database upload
-        python "$pipeline_scripts"/tsv2db.py \
-          --tsvfile analysis/"$sample"/Results/"$sample"/"$sample"_CombinedVariantOutput_padding.tsv \
-          --ntcfile analysis/NTC-"$worksheet_id"/Results/NTC-"$worksheet_id"/NTC-"$worksheet_id"_CombinedVariantOutput_padding.tsv \
-          --outfile ./Gathered_Results/Database/"$sample"_variants_temp.tsv
-
-        # remove any variants called in duplicate (due to re-calling variants beween +/- 2 to 5bp of exons), use head and tail to handle the header
-        head -n1 ./Gathered_Results/Database/"$sample"_variants_temp.tsv > ./Gathered_Results/Database/"$sample"_variants.tsv
-        tail -n +2 ./Gathered_Results/Database/"$sample"_variants_temp.tsv | sort | uniq >> ./Gathered_Results/Database/"$sample"_variants.tsv
-        rm ./Gathered_Results/Database/"$sample"_variants_temp.tsv
-
-        # run coverage scripts for database upload
-        if [[ "$referral" != "null" ]]; then
-            python "$pipeline_scripts"/coverage2json.py \
-              --referral "$referral" \
-              --groups_folder "$pipeline_dir"/hotspot_coverage/ \
-              --sample_coverage analysis/"$sample"/depth_of_coverage/ \
-              --ntc_coverage analysis/NTC-"$worksheet_id"/depth_of_coverage/ \
-              --outfile ./Gathered_Results/Database/"$sample"_"$referral"_coverage.json
-        fi
-
-        # copy BAMs to main folder
-        cp analysis/"$sample"/Logs_Intermediates/StitchedRealigned/"$sample"/"$sample".bam BAMs
-        cp analysis/"$sample"/Logs_Intermediates/StitchedRealigned/"$sample"/"$sample".bam.bai BAMs
-
-        # combined DNA QC files
-        if [[ ! -f DNA_QC_combined.txt ]]; then
-            cat analysis/"$sample"/"$sample"_DNA_QC.txt > DNA_QC_combined.txt
-        else
-            cat analysis/"$sample"/"$sample"_DNA_QC.txt | tail -n1 >> DNA_QC_combined.txt
-        fi
-
-        # make database upload sample list
-        if [[ "$sample" != NTC* ]]; then
-            echo "$line" >> Gathered_Results/Database/samples_database_"$worksheet_id"_DNA.csv
-        fi
-
-    done
-done
-
 
 ##############################################################################################
 #  Generate inputs for variants database - RNA
@@ -180,8 +125,9 @@ for worksheet_id in $(cat worksheets_rna.txt); do
 done
 
 # move sample log files into their own folders
-for sample in $(cat sample_list.txt)
+cat samples_correct_order_*_RNA.csv | while read line
 do
+    sample=$(echo ${line} | cut -f 1 -d ",")
     mv "$sample"_2_TSO500*.out analysis/"$sample"
     mv "$sample"_2_TSO500*.err analysis/"$sample"
 done
