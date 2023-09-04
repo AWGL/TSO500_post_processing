ca84a4108684f190b71ec21b8967318cd8eddbf8
Sophie Shaw <sophie.shaw@wales.nhs.uk>
2023-09-04 15:18:04 +0100

    Version main -> master script 2

--- 2_TSO500.sh
+++ 2_TSO500.sh
@@ -22,7 +22,7 @@ app_version=2.2.0
 app_dir=/data/diagnostics/pipelines/TSO500/illumina_app/TSO500_RUO_LocalApp-"$app_version"
 
 # define filepaths for post processing
-pipeline_version=master
+pipeline_version=main
 pipeline_dir=/data/diagnostics/pipelines/TSO500/TSO500_post_processing-"$pipeline_version"
 pipeline_scripts="$pipeline_dir"/scripts
 
