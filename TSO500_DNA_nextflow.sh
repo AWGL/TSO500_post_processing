f3aa3a4f63209950d3dfb9c8ac9285d82e2b360d
SophieS9 <sophie.shaw@wales.nhs.uk>
2023-10-23 10:11:17 +0100

    Silly typo

--- TSO500_DNA_nextflow.sh
+++ TSO500_DNA_nextflow.sh
@@ -41,7 +41,7 @@ conda deactivate
 set -u
 
 #Only remove work directory if successful pipeline completion
-if [[ `tail -n 1 results\post_processing_finished.txt` == "${SEQID} success!." ]]
+if [[ `tail -n 1 results/post_processing_finished.txt` == "${SEQID} success!." ]]
 then
     rm -r work/
 fi
